8b131a6f828f6b18b15f542c805a8f58d78d1d4e
Victoria Lam <victoria.lam522@gmail.com>
2018-06-19 19:30:58 -0400

    added data table

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var searchBtn = document.querySelector("#search");
 //var $originInput = document.querySelector("#origin");
 var destinationInput = document.getElementById("destination");
 var departureInput = document.getElementById("departure");
+var table = document.getElementById("myTable")
 //var $returnInput = document.querySelector("#return");
 
 // Add event lister to button, call handleSearchButtonClick when clicked
@@ -17,6 +18,8 @@ function handleSearchButtonClick() {
     totalDuration = [];
     nycAirports = [];
 
+    var dataSet = []
+
     //var filterOrigin = $originInput.value.trim().toLowerCase();
     var filterDestination = destinationInput.value.trim().toLowerCase();
     var filterDeparture = departureInput.value.trim();
@@ -24,7 +27,7 @@ function handleSearchButtonClick() {
 
     var key = "PHJmVRmr1DGotzz8YPFk92iTGAl3bGIQ";
     var url = "https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=" + key + "&origin=NYC&destination=" + filterDestination + "&departure_date=" + filterDeparture;
-    //console.log(url);    
+    console.log(url);    
 
     // create a new request object 
     var request = new XMLHttpRequest();
@@ -38,6 +41,7 @@ function handleSearchButtonClick() {
     // process and read request
     request.onreadystatechange = processRequest;
 
+    // get data if api url is good 
     function processRequest(data) {
         if (request.readyState == 4 && request.status == 200) {
             // parse though response and print json in console
@@ -48,7 +52,6 @@ function handleSearchButtonClick() {
 
             for (var i = 0; i < results.length; i++) {
                 var fare = results[i].fare.total_price;
-                //totalFare.push(fare);
 
                 var itineraries = results[i].itineraries
                 //console.log(itineraries)
@@ -62,26 +65,87 @@ function handleSearchButtonClick() {
                     
                     //console.log("Length of travel: " + duration + " minutes")
                     //console.log("Total fare: $" + fare)
-
                     var flights = itineraries[j].outbound.flights
 
                     totalFare.push(fare)
+                    //dataSet.push(fare)
+
                     totalDuration.push(minutes)
-                    
+
                     originAirports = []
                     for (var k = 0; k < flights.length; k++) {
-                        var origin = flights[k].origin.airport
+                        var origin = flights[k].origin.airport;
+                        var departTime = flights[k].departs_at;
+                        var arriveTime = flights[k].arrives_at;
+                        var markAirline = flights[k].marketing_airline;
+                        var opAirline = flights[k].operating_airline;
+                        
+                        departTime = departTime.replace("T", " ");
+                        arriveTime = arriveTime.replace("T", " ");
+
                         //console.log(origin)
-                        originAirports.push(origin)
-                    // console.log(originAirports)
+                        originAirports.push(origin);
+                        
+                        //console.log(dataSet)
+                        // console.log(originAirports)
+                        //console.log(fare)
+                        //console.log("fare: "+fare +"," + "duration: "+duration+","+"origin: "+origin+","+"depart: "+departTime+","+"arrive"+arriveTime+","+"mark: "+markAirline+","+"op: "+opAirline)
                     
-                    }
+                    };
                 nycAirports.push(originAirports[0]);
-                }
-            }
+                dataSet.push([opAirline, markAirline, fare, originAirports, duration, departTime, arriveTime]);
+
+                };
+            };
         // console.log(totalFare);
         // console.log(totalDuration);
         // console.log(nycAirports);
-        }
+        // console.log(dataSet)
+
+        // create flight data table
+        $(document).ready(function() {
+         $("#myTable").DataTable( {
+             destroy: true,
+             data: dataSet,
+             columns : [
+                 {title: "Operating Airline"},
+                 {title: "Marketing Airline"},
+                 {title: "Total Fare ($)"},
+                 {title: "Departure Airport"},
+                 {title: "Duration (hh:mm)"},
+                 {title: "Departing Time"},
+                 {title: "Arrival Time"}
+             ]
+         });
+     });
+
+        // create scatterplot with populated list data    
+        var flightData = [{
+            x: totalDuration,
+            y: totalFare,
+            hovertext: nycAirports,
+            mode: "markers",
+            markers: {
+                opacity: 0.5,
+                size: 20
+            },
+            selected: {
+                marker: {
+                    color: "red",
+                    size: 30
+                }
+            },
+            type: "scatter"
+        }];
+
+        // set up layout for chart
+        var flightLayout = {
+            title: `<b>Available Flights for ${filterDeparture} to ${filterDestination}</b>`,
+            yaxis: {title: "Total One-Way Fare ($)"},
+            xaxis: {title: "Duration of Flight (mins)"}
+        };
+
+        Plotly.newPlot("chart", flightData, flightLayout)
+        }        
     };
 };
