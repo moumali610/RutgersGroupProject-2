0ef6975447f6c888f92ed8813cd4a275c2ba88c4
laptopuser <ces271@rutgers.edu>
2018-07-12 23:43:14 -0400

    adding new aiport information for NY/NJ and starting to update python files

--- app.py
+++ app.py
@@ -29,16 +29,27 @@ db = SQLAlchemy(app)
 
 class Airports(db.Model):
     __tablename__ = 'airports'
-
-    id = db.Column(db.Integer, primary_key=True)
-    iati_code = db.Column(db.String)
-    name = db.Column(db.String)
-    city_code = db.Column(db.String)
-    city_name = db.Column(db.String)
-    country_code = db.Column(db.String)
-    country = db.Column(db.String)
-    lat = db.Column(db.Float)
-    long = db.Column(db.Float)
+        faa_site_number = db.Column(db.String, primary_key=True)
+        loc_id = db.Column(db.String)
+        airport_name = db.Column(db.String)
+        associated_city = db.Column(db.String)
+        state = db.Column(db.String)
+        region = db.Column(db.String)
+        ado = db.Column(db.String)
+        use = db.Column(db.String)
+        lat_dms = db.Column(db.String)
+        long_dms = db.Column(db.String)
+        lat_dd = db.Column(db.Float)
+        long_dd = db.Column(db.Float)
+        airport_ownership = db.Column(db.String)
+        part_139 = db.Column(db.String)
+        npias_service_level = db.Column(db.String)
+        npias_hub_type = db.Column(db.String)
+        airport_control_tower = db.Column(db.String)
+        fuel = db.Column(db.String)
+        other_services = db.Column(db.String)
+        based_aircraft_total = db.Column(db.Integer)
+        total_operations = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Airports %r>' % self.name
@@ -66,18 +77,35 @@ def countries():
 @app.route('/airports')
 def airport():
 
-    sel = [Airports.iati_code, Airports.name, Airports.city_code,Airports.city_name, Airports.country, Airports.lat, Airports.long]
+    sel = [
+        Airports.faa_site_number,
+        Airports.loc_id,
+        Airports.airport_name,
+        Airports.associated_city,
+        Airports.state,
+        Airports.region,
+        Airports.ado,
+        Airports.use,
+        Airports.lat_dms,
+        Airports.long_dms,
+        Airports.lat_dd,
+        Airports.long_dd,
+        Airports.airport_ownership,
+        Airports.part_139,
+        Airports.npias_service_level,
+        Airports.npias_hub_type,
+        Airports.airport_control_tower,
+        Airports.fuel,
+        Airports.other_services,
+        Airports.based_aircraft_total,
+        Airports.total_operations
+        ]
     
     results = db.session.query(*sel).all()
-    df = pd.DataFrame(results, columns=['_iati_code', '_name','city_code','city_name','country','lat','long'])
+    df = pd.DataFrame(results, columns=['faa_site_number', 'loc_id', 'airport_name', 'associated_city', 'state', 'region', 'ado', 'use', 'lat_dms', 'long_dms', 'lat_dd', 'long_dd', 'airport_ownership', 'part_139', 'npias_service_level', 'npias_hub_type', 'airport_control_tower', 'fuel', 'other_services', 'based_aircraft_total', 'total_operations'])
     
     
     return jsonify(df.to_dict(orient="records"))
-
-# create route that renders airports.html template
-@app.route("/map")
-def map():
-    return render_template("airports.html")
 
 # create route that renders flights.html template
 @app.route("/flights")
